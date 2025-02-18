796f6571fa5e8c8d15c6baf62593c3976787b252
aaspellclark-ch <aaspellclark@companieshouse.gov.uk>
2025-02-18 08:26:55 +0000

    add javadoc

--- src/main/java/uk/gov/companieshouse/chs/notification/sender/api/ChsNotificationSenderApiApplication.java
+++ src/main/java/uk/gov/companieshouse/chs/notification/sender/api/ChsNotificationSenderApiApplication.java
@@ -3,6 +3,9 @@ package uk.gov.companieshouse.chs.notification.sender.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the entry point for this service
+ */
 @SpringBootApplication
 public class ChsNotificationSenderApiApplication {
 
