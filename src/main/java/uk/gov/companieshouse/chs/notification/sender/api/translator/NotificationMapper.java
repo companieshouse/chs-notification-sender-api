6f9950acd3abcdb50b3d7da5babad47ce1037557
rnair-ch <rnair@companieshouse.gov.uk>
2025-03-12 12:56:28 +0000

    DEEP-260 updated mapping for createdAt field

--- src/main/java/uk/gov/companieshouse/chs/notification/sender/api/translator/NotificationMapper.java
+++ src/main/java/uk/gov/companieshouse/chs/notification/sender/api/translator/NotificationMapper.java
@@ -1,6 +1,8 @@
 package uk.gov.companieshouse.chs.notification.sender.api.translator;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.chs_notification_sender.model.GovUkEmailDetailsRequest;
@@ -8,14 +10,23 @@ import uk.gov.companieshouse.api.chs_notification_sender.model.GovUkLetterDetail
 import uk.gov.companieshouse.notification.ChsEmailNotification;
 import uk.gov.companieshouse.notification.ChsLetterNotification;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+
 @Component
 @Mapper(componentModel = "spring")
 interface NotificationMapper {
 
     NotificationMapper INSTANCE = Mappers.getMapper(NotificationMapper.class);
 
+    @Mapping(source = "createdAt", target = "createdAt", qualifiedByName = "offsetDateTimeToInstant")
     ChsEmailNotification mapToEmailDetailsRequest(GovUkEmailDetailsRequest govUkEmailDetailsRequest);
 
+    @Mapping(source = "createdAt", target = "createdAt", qualifiedByName = "offsetDateTimeToInstant")
     ChsLetterNotification mapToLetterDetailsRequest(GovUkLetterDetailsRequest govUkLetterDetailsRequest);
 
+    @Named("offsetDateTimeToInstant")
+    static Instant offsetDateTimeToInstant(OffsetDateTime dateTime) {
+        return dateTime != null ? dateTime.toInstant() : null;
+    }
 }
