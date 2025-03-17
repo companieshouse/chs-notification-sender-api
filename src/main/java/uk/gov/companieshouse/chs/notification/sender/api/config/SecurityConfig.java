54ca01236bd7d6e743c8af0b0e7b9457832deae8
mcarey-ch <mcarey@companieshouse.gov.uk>
2025-03-17 14:09:38 +0000

    Visit security config

--- src/main/java/uk/gov/companieshouse/chs/notification/sender/api/config/SecurityConfig.java
+++ src/main/java/uk/gov/companieshouse/chs/notification/sender/api/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package uk.gov.companieshouse.chs.notification.sender.api.config;
 
+import static uk.gov.companieshouse.csrf.config.ChsCsrfMitigationHttpSecurityBuilder.configureApiCsrfMitigations;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.http.HttpMethod.GET;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,19 +16,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Order(1)
     @Bean
-    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChainActuator(final HttpSecurity http) throws Exception {
         http
             .cors(AbstractHttpConfigurer::disable)
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(request -> request
-                .requestMatchers(GET, "/actuator/**")
-                .permitAll()
-                .anyRequest()
-                .permitAll())
-        ;
-        return http.build();
+            .securityMatcher("/actuator/**");
+        return configureApiCsrfMitigations(http).build();
     }
 
+    @Order(2)
+    @Bean
+    public SecurityFilterChain filterChainNotificationSender(final HttpSecurity http) throws Exception {
+        http
+            .cors(AbstractHttpConfigurer::disable)
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .securityMatcher("/notification-sender/**")
+            // some possible further granularity in control
+            //.authorizeHttpRequests(req -> req.requestMatchers("/notification-sender/**").hasRole("NOTIFIER"))
+            ;
+        return configureApiCsrfMitigations(http).build();
+    }
 }
