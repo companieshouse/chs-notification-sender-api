5453a19bd294b9d893bc97228eb5391034ce916c
mcarey-ch <mcarey@companieshouse.gov.uk>
2025-03-18 12:59:21 +0000

    Added test for my sanity

--- src/test/java/uk/gov/companieshouse/chs/notification/sender/api/ChsNotificationSenderApiApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/chs/notification/sender/api/ChsNotificationSenderApiApplicationTests.java
@@ -1,11 +1,23 @@
 package uk.gov.companieshouse.chs.notification.sender.api;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChsNotificationSenderApiApplicationTests {
+
+   @Value("${test.property.value}")
+     private String testPropertyValue;
+
+    @Test
+    void testDefaultPropertiesFile() {
+        assertThat(testPropertyValue)
+            .as("Default property value should match")
+            .isEqualTo("default-value");
+    }
 
     @Test
     void contextLoads() {
