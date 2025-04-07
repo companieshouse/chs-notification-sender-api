db4de944aeb9ec8f302b4e46b5d323c00998937d
tferns-ch <tferns@companieshouse.gov.uk>
2025-04-07 16:32:21 +0100

    refactor(DEEP-BAU): Update healthcheck test

--- src/test/java/uk/gov/companieshouse/chs/notification/sender/api/health/HealthCheckControllerTest.java
+++ src/test/java/uk/gov/companieshouse/chs/notification/sender/api/health/HealthCheckControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,10 +17,17 @@ public class HealthCheckControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    private final String healthcheckUrl;
+
+    public HealthCheckControllerTest(
+            @Value("${management.endpoints.web.path-mapping.health}") final String healthcheckUrl) {
+        this.healthcheckUrl = healthcheckUrl;
+    }
 
     @Test
     public void checkHealthEndpointReturnsOk() throws Exception {
-        mockMvc.perform(get("/healthcheck"))
+        mockMvc.perform(get(healthcheckUrl))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value("UP"));
     }
